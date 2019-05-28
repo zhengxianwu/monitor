2dd308930cef4e3dec4dc0db5ae51c1735bc5e9b
wuzhengxian <Wuzhengxian1996@outlook.com>
2019-05-28 17:15:21 +0800

    上传新的mysql文件
    跟新了schedule表结构

--- monitor.sql
+++ monitor.sql
@@ -1,5 +1,5 @@
 ﻿# Host: localhost  (Version: 5.5.53)
-# Date: 2019-05-10 09:50:41
+# Date: 2019-05-28 17:08:06
 # Generator: MySQL-Front 5.3  (Build 4.234)
 
 /*!40101 SET NAMES utf8 */;
@@ -33,13 +33,13 @@ CREATE TABLE `nailing_robot` (
   `rootName` varchar(255) NOT NULL DEFAULT '' COMMENT '机器人名字',
   `rootToken` varchar(255) NOT NULL DEFAULT '' COMMENT '机器人Token',
   PRIMARY KEY (`Id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 #
 # Data for table "nailing_robot"
 #
 
-INSERT INTO `nailing_robot` VALUES (2,'5ecef153b23c439d2c629d78e4d10ff8','开发测试机器人','2dced5c5d02e3a8b2769e61236bba976ab3dce7eb6de7883b14c2617145f7006');
+INSERT INTO `nailing_robot` VALUES (2,'5ecef153b23c439d2c629d78e4d10ff8','开发测试机器人','2dced5c5d02e3a8b2769e61236bba976ab3dce7eb6de7883b14c2617145f7006'),(3,'ecb463b4a0c0664500238a93c7bd3be3','钉钉测试','2dced5c5d02e3a8b2769e61236bba976ab3dce7eb6de7883b14c2617145f7006');
 
 #
 # Structure for table "schedule"
@@ -51,19 +51,20 @@ CREATE TABLE `schedule` (
   `type` varchar(255) NOT NULL DEFAULT '' COMMENT '监控类型',
   `threshold` varchar(255) NOT NULL DEFAULT '0' COMMENT '阈值',
   `taskId` varchar(255) NOT NULL DEFAULT '0' COMMENT '定时任务Id',
+  `taskName` varchar(255) NOT NULL DEFAULT '' COMMENT '任务名称',
   `taskType` varchar(255) NOT NULL DEFAULT '' COMMENT '定时类型（秒，分钟，小时，天）',
   `taskValue` varchar(255) NOT NULL DEFAULT '' COMMENT '任务时间',
   `taskState` varchar(255) NOT NULL DEFAULT '' COMMENT '任务状态（运行，暂停）',
   `operationType` varchar(255) NOT NULL DEFAULT '' COMMENT '存储判断监控（比如大于等于，小于等于）',
   `ReminderType` varchar(255) DEFAULT '' COMMENT '通知类型DingTalkRobot("钉钉机器人"), EMail("邮件通知"),SMS("手机短信");',
   `ReminderId` varchar(255) NOT NULL DEFAULT '' COMMENT '通知Id',
-  `customExpression` varchar(255) DEFAULT NULL COMMENT '自定义通知表达式',
+  `customExpression` varchar(255) DEFAULT NULL COMMENT '自定义通知表达式，空则默认',
   PRIMARY KEY (`Id`),
   UNIQUE KEY `taskId` (`taskId`)
-) ENGINE=MyISAM AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=42 DEFAULT CHARSET=utf8;
 
 #
 # Data for table "schedule"
 #
 
-INSERT INTO `schedule` VALUES (32,'任务分钟','Cpu','10','20f3f4bfa777c6c567b22c1c6b7f7130','Minute','1','Stop','','','',NULL),(34,'任务小时','Cpu','10','c2d10af9e45eddcb298b8d1e1da04345','Hour','1','Stop','','','',NULL),(37,'任务H_M_S','Cpu','10','a461d577813852c0c7da6b3cc5067d95','H_M_S','1_1_2','Stop','','','',NULL),(38,'zhengxian','Memory','32','274d0d8ee61aef85d8838ac035788c21','Second','20','Stop','Gte','DingTalkRobot','5ecef153b23c439d2c629d78e4d10ff8','主机名称 ： {主机名称}\n监控类型 ：{监控类型}\n超过阈值数量 :{超过阈值数量}\n超过记录数据 ：{超过记录数据}'),(39,'zhengxian','Memory','43','d46142c6e138052fbd4311549c219aa9','Second','60','Stop','Gte','DingTalkRobot','5ecef153b23c439d2c629d78e4d10ff8','主机名称 ： {主机名称}\n监控类型 ：{监控类型}\n超过阈值数量 :{超过阈值数量}\n超过记录数据 ：{超过记录数据}');
+INSERT INTO `schedule` VALUES (38,'zhengxian','Cpu','60','274d0d8ee61aef85d8838ac035788c21','任务名称测试1','Hour','10','Stop','Gte','DingTalkRobot','5ecef153b23c439d2c629d78e4d10ff8','{主机名称}'),(39,'zhengxian','Memory','43','d46142c6e138052fbd4311549c219aa9','内存定时任务','Second','60','Stop','Gte','DingTalkRobot','5ecef153b23c439d2c629d78e4d10ff8','主机名称 ： {主机名称}\n监控类型 ：{监控类型}\n超过阈值数量 :{超过阈值数量}\n超过记录数据 ：{超过记录数据}'),(41,'zhengxian','Filesystem','31','dd9de33d15fba516846d065aae5f33f5','添加测试','Minute','123','Stop','Lte','DingTalkRobot','ecb463b4a0c0664500238a93c7bd3be3','123123');
